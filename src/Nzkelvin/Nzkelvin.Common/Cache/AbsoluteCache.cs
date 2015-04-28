07d1f9cfe07dffe12fd8afeef88348aee1c5268b
Kelvin Shen <kelvinshen@outlook.com>
2015-04-29 06:43:59 +1200

    Add two missing classes

---
+++ src/Nzkelvin/Nzkelvin.Common/Cache/AbsoluteCache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace Nzkelvin.Common.Cache
+{
+    public class AbsoluteCache : ICache
+    {
+        private MemoryCache _cache = MemoryCache.Default;
+        private CacheItemPolicy _defaultPolicy;
+
+        public AbsoluteCache()
+        {
+            _defaultPolicy = new CacheItemPolicy()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(15),
+                Priority = CacheItemPriority.Default
+            };
+        }
+
+        public void Add<T> (string key, T value)
+        {
+            Add(key, value, _defaultPolicy);
+        }
+
+        public void Add<T>(string key, T value, CacheItemPolicy policy)
+        {
+            _cache.Add(key, value, policy);
+        }
+
+        public T Get<T> (string key)
+        {
+            return (T)_cache.Get(key);
+        }
+    }
+}
---
+++ src/Nzkelvin/Nzkelvin.Common/Cache/ICache.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nzkelvin.Common.Cache
+{
+    public interface ICache
+    {
+        void Add<T>(string key, T value);
+        T Get<T>(string key);
+    }
+
+    public static class Extensions
+    {
+        private static string TRANSACTION_LOCK_PREFIX = "cacheItemTransactionLock_";
+
+        //To use this method. You make a new method on the fly and use the method immediately.
+        /*
+         var getRules = _cacheService.Retrieve<string, ApplicationValidationRules>(key => key, key => GetApplicationValidationRules())(CACHE_KEY_PREFIX + "AppValidationRules");
+         */
+        public static Func<TArg, TResult> Retrieve<TArg, TResult>(this ICache cacheService,
+                                                                    Func<TArg, string> createCacheKey,
+                                                                    Func<TArg, TResult> getItem)
+        {
+            return arg =>
+            {
+                var key = createCacheKey(arg);
+                var lockKey = TRANSACTION_LOCK_PREFIX + key;
+
+                var value = cacheService.Get<TResult>(key);
+                if (value == null)
+                {
+                    var lockObj = GetTransactionLockObject(lockKey, cacheService); // Garenteed fast
+                    lock (lockObj)
+                    {
+                        if (value == null)
+                        {
+                            value = getItem(arg); // Potentially can be very slow.
+                            cacheService.Add<TResult>(key, value);
+                        }
+                    }
+                }
+
+                return value;
+            };
+        }
+
+        private static Object GetTransactionLockObject(string lockObjKey, ICache cacheService)
+        {
+            var lockObj = cacheService.Get<Object>(lockObjKey);
+            if (lockObj != null)
+                return lockObj;
+
+            lockObj = new Object();
+            cacheService.Add(lockObjKey, lockObj);
+            return lockObj;
+        }
+    }
+}
